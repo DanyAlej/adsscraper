4181b45da48b7bd4ccce2dd1328a7edb629b5f8c
DanyAlej <da.benavides@uniandes.edu.co>
2020-07-17 07:01:23 -0500

    One ad scraped scraper and 3 attributes arrays consistent

--- adsscraper.py
+++ adsscraper.py
@@ -52,18 +52,64 @@ def scrollToBotton():
     driver.execute_script("window.scrollTo(0, 0)")
 
 def clickAllAds():
-    ads_text_div = driver.find_elements_by_xpath('//div[starts-with(@class,"_7jyr")]')
+    ads_copy_div = driver.find_elements_by_xpath('//div[@class="_7jyr"]')
     time.sleep(3)
-    for ad in ads_text_div:
-        ad.click()
-        time.sleep(randint(3,6))
-    
+    for ad in ads_copy_div:
+        try:
+            ad.click()
+            time.sleep(1.5)
+        except:
+            ads_copy_div.remove(ad)
+    driver.execute_script("window.scrollTo(0, 0)")
+    return ads_copy_div
 
+
+def peristAds():
+    started_running = driver.find_elements_by_xpath('//div[@class="_7jwu"]')
+    #platforms = driver.find_element_by_xpath('//div[starts-with(@class,"_3qn7")]').find_element_by_xpath('//i').get_attribute("class")
+    platforms = driver.find_elements_by_xpath('//div[@class="_8k-_"]')
+    #print('Facebook' if platform == 'img sp_qqiEEYnS8_M sx_b88b1a' else 'Other platform')
+    ads_copy_div = driver.find_elements_by_xpath('//div[@class="_7jyr"]')
+    headlines = driver.find_elements_by_xpath('//div[@class="_8jh2"]')
+    #driver.find_element_by_xpath('//a[starts-with(@class,"_231w")]').get_property('href')
+    #driver.find_element_by_xpath('//img[starts-with(@class,"_7jys")]').get_property('src')
+    urls = driver.find_elements_by_xpath('//img[starts-with(@class,"_7jys")]')
+
+    creative_urls = driver.find_element_by_xpath('//img[starts-with(@class,"_7jys")]').get_property('src')
+    # creative_url = driver.find_element_by_xpath('//img[starts-with(@class,"_7jys")]').get_property('src')
+    # counter = 1
+    # file_creative_name = "SamOvens_creative" + str(counter)
+    # r = requests.get(creative_url, stream = True)
+    # r.raw.decode_content = True
+    # print(file_creative_name)
+    count = 0
+    newArrayOfCopy = []
+    for copy in ads_copy_div:
+        if (copy.is_displayed() == True): 
+            newArrayOfCopy.append(copy)
+
+    print("Started runnign date: " + str(len(started_running)))
+    print("Platforms: " + str(len(platforms)))
+    print("Ads Copy: " + str(len(newArrayOfCopy)))
+    print("Headlines: " + str(len(headlines)))
+    print("URLS: " + str(len(urls)))
+    print("Creative urls: " + str(len(creative_urls)))
+    printArrayText(newArrayOfCopy)
+
+def printArrayText(array):
+    for element in array:
+        print(element.text)
+        print(len(element.text))
+        print(element.is_displayed())
+        print(element.is_enabled())
+        print("--------------------------")
+        print("--------------------------")
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 loadAds()
 scrollToBotton()
-clickAllAds()
+ads = clickAllAds()
+peristAds()
 
 driver.close()
---
+++ onead.py
@@ -0,0 +1,80 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.action_chains import ActionChains
+from random import randint
+from pymongo import MongoClient
+from bson.binary import Binary
+import sys
+from selenium.webdriver.common.keys import Keys
+import requests
+
+class ad():
+    pass
+
+def loadAds():
+    driver.get("https://www.facebook.com/ads/library/")
+    time.sleep(4)
+    search_bar = driver.find_element_by_xpath('//input[starts-with(@type,"text")]')
+    time.sleep(1)
+    search_bar.send_keys("Sam Ovens")
+    time.sleep(3)
+
+    driver.find_element_by_xpath('//div[starts-with(@class,"_7h2l")]').click()
+    time.sleep(3)
+
+    driver.find_element_by_xpath('//button[starts-with(@type,"button")]').click()
+    time.sleep(3)
+
+    driver.find_element_by_xpath('//li[starts-with(@data-testid,"all-ad-type-tab")]').click()
+    time.sleep(3)
+
+    driver.find_element_by_xpath('//span[starts-with(@class,"_7fc8")]').click()
+    time.sleep(3)
+
+    driver.find_element_by_xpath('//div[starts-with(@class,"ellipsis")]').click()
+    time.sleep(3)
+
+
+    search_bar = driver.find_element_by_xpath('//input[starts-with(@type,"text")]')
+    search_bar.click()
+    time.sleep(2)
+
+    driver.find_element_by_xpath('//div[starts-with(@class,"_7h2l")]').click()
+    time.sleep(3)
+
+
+def clickOneAd():
+    ad = driver.find_element_by_xpath('//div[@class="_7jyr"]')
+    time.sleep(3)
+    ad.click()
+    time.sleep(randint(3,6))
+
+def peristAds():
+    ad = driver.find_element_by_xpath('//div[@class="_7jyr"]')
+    print(driver.find_element_by_xpath('//div[@class="_7jwu"]').text)
+    platform = driver.find_element_by_xpath('//div[starts-with(@class,"_3qn7")]').find_element_by_xpath('//i').get_attribute("class")
+    print('Facebook' if platform == 'img sp_qqiEEYnS8_M sx_b88b1a' else 'Other platform')
+    print(driver.find_element_by_xpath('//div[@class="_7jyr"]').text)
+    print(driver.find_element_by_xpath('//div[@class="_8jh2"]').text)
+    print(driver.find_element_by_xpath('//a[starts-with(@class,"_231w")]').get_property('href'))
+    print(driver.find_element_by_xpath('//img[starts-with(@class,"_7jys")]').get_property('src'))
+
+    creative_url = driver.find_element_by_xpath('//img[starts-with(@class,"_7jys")]').get_property('src')
+    counter = 1
+    file_creative_name = "SamOvens_creative" + str(counter)
+    r = requests.get(creative_url, stream = True)
+    r.raw.decode_content = True
+    print(file_creative_name)
+
+
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+loadAds()
+clickOneAd()
+peristAds()
+
+driver.close()
